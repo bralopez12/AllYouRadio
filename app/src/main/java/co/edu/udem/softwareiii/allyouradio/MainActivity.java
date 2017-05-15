f79ae42328831e0b3a96770b56a20a939f199f47
bralopez12 <bralopez12@hotmail.com>
2017-05-15 12:29:02 -0500

    Update MainActivity.java

--- app/src/main/java/co/edu/udem/softwareiii/allyouradio/MainActivity.java
+++ app/src/main/java/co/edu/udem/softwareiii/allyouradio/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView title;
+    //TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
