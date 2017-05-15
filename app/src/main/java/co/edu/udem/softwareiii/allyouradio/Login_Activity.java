6f236eaa68ceb52f9e3f66ef5a7ddbd8183a1367
bralopez12 <bralopez12@hotmail.com>
2017-05-15 15:49:46 -0500

    add Instance items login_activity

--- app/src/main/java/co/edu/udem/softwareiii/allyouradio/Login_Activity.java
+++ app/src/main/java/co/edu/udem/softwareiii/allyouradio/Login_Activity.java
@@ -4,13 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login_Activity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity implements View.OnClickListener {
+
+
+    EditText etUsuario, etPassword;
+    Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsuario = (EditText)findViewById(R.id.etUsuario);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+
+        btnSignIn = (Button)findViewById(R.id.btnSingIn);
+        btnSignUp = (Button)findViewById(R.id.btnSingUp);
+
+        btnSignIn.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+
+
+
     }
 
     @Override
@@ -29,9 +50,38 @@ public class Login_Activity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(Login_Activity.this, "RTF 4 - ING.SOFTWARE III", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Login_Activity.this, "Universidad de Medellín", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Login_Activity.this, "Por:", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Login_Activity.this, "Andres Felipe Granda", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Login_Activity.this, "Sebastian Moncada", Toast.LENGTH_SHORT).show();
+            Toast.makeText(Login_Activity.this, "Brayan López", Toast.LENGTH_SHORT).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+
+
+            case R.id.btnSingIn:
+
+                break;
+
+
+            case R.id.btnSingUp:
+
+                break;
+
+
+
+        }
+
+
     }
 }
