cbf5bb03e85f10aeec4948cbf0800fd4da89d4d3
bralopez12 <bralopez12@hotmail.com>
2017-05-15 12:26:23 -0500

    prueba de push

--- app/src/main/java/co/edu/udem/softwareiii/allyouradio/MainActivity.java
+++ app/src/main/java/co/edu/udem/softwareiii/allyouradio/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
