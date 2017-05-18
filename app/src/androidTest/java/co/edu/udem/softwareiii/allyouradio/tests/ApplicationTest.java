fd6cde1cb4d4ec1dd31ab6d452a2554638b93ed1
bralopez12 <bralopez12@hotmail.com>
2017-05-17 19:44:07 -0500

    new tests

---
+++ app/src/androidTest/java/co/edu/udem/softwareiii/allyouradio/tests/ApplicationTest.java
@@ -0,0 +1,26 @@
+package co.edu.udem.softwareiii.allyouradio.tests;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import co.edu.udem.softwareiii.allyouradio.Activitys.RegisterListener_Activity;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+
+    public void test_validacion_Registro_Oyente(){
+
+        RegisterListener_Activity ra = new RegisterListener_Activity();
+
+        assertTrue(ra.ValidarDatosOyente("bralopez12","brayan lopez foronda","queleimporta"));
+        assertTrue(ra.ValidarDatosOyente("bralopez12@hotmail.com","andres felipe granda","teamoyurani"));
+
+
+    }
+
+}
